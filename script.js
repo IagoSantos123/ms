23daa1502178e5d52c204799289d0c0076bec61f
Iago Santos <is0941011@gmail.com>
2024-03-29 10:29:16 -0300

    att

--- script.js
+++ script.js
@@ -11,17 +11,18 @@ $(document).ready(function(){
             $('.scroll-up-btn').removeClass("show");
         }
     });
-     $('.scroll-up-btn').click(function(){
-         $('html').animate({scrollTop: 0});
-     });
 
-     var typed = new Typed(".typing", {
-         strings:["Desenvolvimento", "Designer", "Marketing"],
-         typeSpeed:100,
-         backSpeed:60,
-         loop:true
-     });
-     var typed = new Typed(".typing-2", {
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    var typed = new Typed(".typing", {
+        strings:["Desenvolvimento", "Designer", "Marketing"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+    var typed = new Typed(".typing-2", {
         strings:["Desenvolvimento", "Designer", "Marketing"],
         typeSpeed:100,
         backSpeed:60,
@@ -32,6 +33,7 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
     $('.carousel').owlCarousel({
         margin:20,
         loop:true,
@@ -51,5 +53,16 @@ $(document).ready(function(){
                 nav:false
             }
         }
+    });
+
+    // Manipulador de eventos para o clique no botão "FALE CONOSCO"
+    $('a[href="https://wa.me/5583998545395"]').click(function(){
+        // Você pode adicionar um console.log para verificar se o manipulador de eventos está sendo acionado
+        console.log("Clique no botão 'FALE CONOSCO'");
+
+        // Adicione qualquer outra lógica que você deseja executar quando o botão é clicado
+
+        // Este return false evita que o link padrão seja seguido. Se não quiser isso, remova esta linha.
+        return false;
     });
 });
